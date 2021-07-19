5c356122d34473759d7a6f89a9839f4db542aba0
yerimkoko <gochi97@naver.com>
2021-07-19 16:55:51 +0900

    [YERIM-20] feat: secret

--- src/main/java/com/example/potato2teambackend/service/TokenService.java
+++ src/main/java/com/example/potato2teambackend/service/TokenService.java
@@ -3,14 +3,17 @@ package com.example.potato2teambackend.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TokenService {
+    @Value("${jwt.secret}")
+    private String secret;
 
     public String createToken(Long memberId) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("secret");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("yerimkoko")
                     .withClaim("memberId", memberId)
