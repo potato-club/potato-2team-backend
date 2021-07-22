94706e0470cae9e4fef161b0292b788c311ca97f
yerimkoko <gochi97@naver.com>
2021-07-22 19:17:53 +0900

    [YERIM-23.5] feat: createTODO (인증), test코드 아직x

--- src/main/java/com/example/potato2teambackend/domain/todo/BoardRepository.java
+++ src/main/java/com/example/potato2teambackend/domain/todo/BoardRepository.java
@@ -1,6 +1,5 @@
 package com.example.potato2teambackend.domain.todo;
 
-import com.example.potato2teambackend.dto.BoardCreateRequestDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
