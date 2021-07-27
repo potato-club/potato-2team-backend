c12975be04c328e4a4a9f4368d29d7d69b1dc09a
yerimkoko <gochi97@naver.com>
2021-07-27 19:53:52 +0900

    [YERIM-33] 테스트 코드 리팩토링

--- src/test/java/com/example/potato2teambackend/service/BoardServiceTest.java
+++ src/test/java/com/example/potato2teambackend/service/BoardServiceTest.java
@@ -5,14 +5,11 @@ import com.example.potato2teambackend.domain.member.MemberRepository;
 import com.example.potato2teambackend.dto.BoardCreateRequestDto;
 import com.example.potato2teambackend.domain.todo.Board;
 import com.example.potato2teambackend.domain.todo.BoardRepository;
-import com.example.potato2teambackend.dto.LoginRequestDto;
-import com.example.potato2teambackend.dto.MemberJoinRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
 
 import static com.example.potato2teambackend.domain.todo.BoardColor.BLUE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,12 +22,6 @@ public class BoardServiceTest {
     @Autowired
     private BoardRepository boardRepository;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private MemberService memberService;
-
     @AfterEach
     public void cleanUp() {
         boardRepository.deleteAll();
@@ -39,21 +30,7 @@ public class BoardServiceTest {
     @Test
     void 글을_저장한다() {
         // given
-        String name = "고예림";
-        String email = "gochi97@naver.com";
-        String password = "234234234";
-
-        MemberJoinRequestDto joinRequestDto = MemberJoinRequestDto.builder()
-                .birth(LocalDate.of(1997,02,17))
-                .name(name)
-                .password(password)
-                .email(email)
-                .build();
-
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .email(email)
-                .password(password)
-                .build();
+        Member member = Member.builder().build();
 
         BoardCreateRequestDto dto = BoardCreateRequestDto.builder()
                 .content("글을 저장합니다.")
@@ -61,19 +38,13 @@ public class BoardServiceTest {
                 .build();
 
         // when
-        memberService.joinMember(joinRequestDto);
-        Long memberId = memberService.loginMember(loginRequestDto);
-        boardService.save(dto, memberId);
+        boardService.save(dto, member.getId());
 
         // then
-        Member member = memberRepository.findByEmail(email);
-        assertThat(member.getEmail()).isEqualTo(joinRequestDto.getEmail());
-        assertThat(member.getEmail()).isEqualTo(loginRequestDto.getEmail());
-
         Board todo = boardRepository.findAll().get(0);
         assertThat(todo.getContent()).isEqualTo(dto.getContent());
         assertThat(todo.getColor()).isEqualTo(dto.getColor());
-        assertThat(todo.getMemberId()).isEqualTo(memberId);
+        assertThat(todo.getMemberId()).isEqualTo(member.getId());
 
     }
 
