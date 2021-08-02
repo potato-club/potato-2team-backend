7c77c76418c99dc3daa48ab2c74546618e2c74d2
yerimkoko <gochi97@naver.com>
2021-08-03 01:29:48 +0900

    [YERIM-41] refactoring date.js

--- src/main/resources/static/date.js
+++ src/main/resources/static/date.js
@@ -1,77 +1,8 @@
-function getDiff() {
-    const currDay = new Date();
-    const birthDay = new Date(`${currDay.getFullYear()}-08-10`);
-
-    const diffDays = Math.floor((birthDay.getTime() - currDay.getTime()) / (1000 * 60 * 60 * 24));
-    document.write(`생일까지 ${diffDays < 10 ? `0${diffDays}` : diffDays}일 남았습니다.`);
-}
-
-
-function getTime() {
-    const xmasDay = new Date("2021-08-10");
-    const currDay = new Date();
-
-    let diff = xmasDay - currDay;
-    const diffDays = Math.floor((xmasDay.getTime() - currDay.getTime()) / (1000 * 60 * 60 * 24));
-    diff -= diffDays * (1000 * 60 * 60 * 24);
-    const diffHours = Math.floor(diff / (1000 * 60 * 60));
-    diff -= diffHours * (1000 * 60 * 60);
-    const diffMin = Math.floor(diff / (1000 * 60));
-    diff -= diffMin * (1000 * 60);
-    const diffSec = Math.floor(diff / 1000);
-
-    document.write(`${diffDays < 10 ? `0${diffDays}` : diffDays}일 ${diffHours < 10 ? `0${diffHours}` : diffHours}시간 ${diffMin < 10 ? `0${diffMin}` : diffMin}분 ${diffSec < 10 ? `0${diffSec}` : diffSec}초`);
-
-}
-
-// const countDownTimer = function (id, date) {
-//     var _vDate = new Date(date); // 전달 받은 일자
-//     var _second = 1000;
-//     var _minute = _second * 60;
-//     var _hour = _minute * 60;
-//     var _day = _hour * 24;
-//     var timer;
-//
-//     function showRemaining() {
-//         const now = new Date();
-//         var distDt = _vDate - now;
-//         if (distDt < 0) {
-//             clearInterval(timer);
-//             document.getElementById(id).textContent = '오늘은 민정이의 생일입니다!';
-//             return;
-//         }
-//         const days = Math.floor(distDt / _day);
-//         const hours = Math.floor((distDt % _day) / _hour);
-//         const minutes = Math.floor((distDt % _hour) / _minute);
-//         const seconds = Math.floor((distDt % _minute) / _second);
-//         document.getElementById(id).textContent = date.toLocaleString() + "까지 : ";
-//         document.getElementById(id).textContent = days + '일 ';
-//         document.getElementById(id).textContent += hours + '시간 ';
-//         document.getElementById(id).textContent += minutes + '분 ';
-//         document.getElementById(id).textContent += seconds + '초';
-//     }
-//
-//     timer = setInterval(showRemaining, 1000);
-// }
-// const dateObj = new Date();
-// dateObj.setDate(dateObj.getDate() + 1);
-// countDownTimer('sample01', dateObj); // 내일까지
-// countDownTimer('sample02', '08/10/2021 00:00 AM'); // 2024년 4월 1일까지, 시간을 표시하려면 01:00 AM과 같은 형식을 사용한다.
-// countDownTimer('sample03', '04/01/2024');// 2024년 4월 1일까지
-// countDownTimer('sample04', '04/01/2019');// 2024년 4월 1일까지
-
-CountDownTimer('12/25/2019', 'countdown');
-
-CountDownTimer('3/19/2019 5:00 PM', 'HourCountdown');
-
-CountDownTimer('2/19/2019', 'countExpire');
-
-
+CountDownTimer('08/10/2021', 'countdown');
 
 function CountDownTimer(dt, id) {
 
     var end = new Date(dt);
-
 
 
     var _second = 1000;
@@ -85,16 +16,12 @@ function CountDownTimer(dt, id) {
     var timer;
 
 
-
     function showRemaining() {
 
         var now = new Date();
 
         var distance = end - now;
 
-
-
-        // 시간 종료 시 뜨는 문구
 
         if (distance < 0) {
 
@@ -113,7 +40,6 @@ function CountDownTimer(dt, id) {
         var minutes = Math.floor((distance % _hour) / _minute);
 
         var seconds = Math.floor((distance % _minute) / _second);
-
 
 
         document.getElementById(id).innerHTML = '민정이의 생일까지 ' + days + '일 ';
