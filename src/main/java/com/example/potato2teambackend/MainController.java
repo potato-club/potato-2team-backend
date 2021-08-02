e4742d75a7a23fc664ff21b82b318e673727f7ea
yerimkoko <gochi97@naver.com>
2021-08-02 20:08:42 +0900

    [YERIM-37] feat: 졸귀 민정이

--- src/main/java/com/example/potato2teambackend/MainController.java
+++ src/main/java/com/example/potato2teambackend/MainController.java
@@ -1,14 +1,20 @@
 package com.example.potato2teambackend;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "index.html";
     }
 
 }
